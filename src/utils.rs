def0803e96a455e441c074e3452863095e6c1e2b
Manuel Schölling <manuel.schoelling@gmx.de>
2015-02-19 21:17:53 +0100

    Keep up with Rust dev

--- src/utils.rs
+++ src/utils.rs
@@ -4,5 +4,10 @@ use std::thread;
 pub fn spawn_thread<'a, F>(name: &'a str, func: F) -> thread::Thread
 		where F: FnOnce(), F: Send + 'static
 {
-	thread::Builder::new().name(name.to_string()).spawn(func)
+	thread::Builder::new()
+		.name(name.to_string())
+		.spawn(func)
+		.unwrap()
+		.thread()
+		.clone()
 }
